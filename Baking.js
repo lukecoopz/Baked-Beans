7bcb2228360b065457e3f7f49c08cbc5177d6911
Luke Cooper <Lukecoopz@gmail.com>
2022-04-20 16:56:05 +1200

    Update Baking.js

--- Baking.js
+++ Baking.js
@@ -3,6 +3,6 @@ const puppeteer = require('puppeteer');
 (async () => {
 const browser = await puppeteer.launch({headless:false, executablePath: 'C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe'});
   const page = await browser.newPage();
-  await page.setViewport({width: 900, height: 800});
+  await page.setViewport({width: 1080, height: 1000});
   await page.goto('https://bakedbeans.io');
 })();
