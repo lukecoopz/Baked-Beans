5f4bb0e38270980309b878639a453ce98a45b0c4
Luke Cooper <Lukecoopz@gmail.com>
2022-04-20 16:41:33 +1200

    code runs now

--- Baking.js
+++ Baking.js
@@ -1,14 +1,15 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({headless:false, args:['--disable-extensions-except=/path/to/my/extension',
-  '--load-extension=/path/to/my/extension',
-'--user-data-dir=%userprofile%\\AppData\\Local\\BraveSoftware\\Brave-Browser\\User Data\\Profile 1'],
-//'--profile-directory=Profile 1'],
-    executablePath: 'C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe',
-  });
+//   const browser = await puppeteer.launch({headless:false, args:['--disable-extensions-except=/path/to/my/extension',
+//   '--load-extension=/path/to/my/extension',
+// '--user-data-dir=%userprofile%\\AppData\\Local\\BraveSoftware\\Brave-Browser\\User Data\\Profile 1'],
+// //'--profile-directory=Profile 1'],
+//     executablePath: 'C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe',
+//   });
+
+const browser = await puppeteer.launch({headless:false, executablePath: 'C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe'});
   const page = await browser.newPage();
-  await page.setViewport({width: 1200, height: 1080, deviceScaleFactor: 1})
+  //await page.setViewport({width: 900, height: 800});
   await page.goto('https://bakedbeans.io');
-  await page.screenshot({ path: 'example.png' });
-})
+})();
